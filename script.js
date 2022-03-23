16132db84cfe26140ba26ee30e05673e2c924ca7
waremxn <102222315+waremxn@users.noreply.github.com>
2022-03-23 19:30:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,70 @@
 
+// alert('ohha');
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+            if(this.scrollY > 20){
+                $('.navbar').addClass("sticky");
+            }else{
+                $('.navbar').removeClass("sticky");
+            }
+            if(this.scrollY > 500){
+                $('.scroll-up-btn').addClass("show");
+            }else{
+                $('.scroll-up-btn').removeClass("show");
+            }
+    });
+    // slide up function
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    // menu navbar togle
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    // typing animation
+    var typed = new Typed(".typing",{
+        strings: ["Coming soon!","as soon as posible!"],
+        typespeed:100,
+        backSpeed:60,
+        loop:true
+
+    })
+    var typed = new Typed(".typing-2",{
+        strings: ["work?","Business?"],
+        typespeed:100,
+        backSpeed:60,
+        loop:true
+
+    })
+
+    // owl carausel
+    $('.carousel').owlCarousel({
+        margin:20,
+        loop:true,
+        autoplayTimeOut:2000,
+        autoplayHoverPause:true,
+        responsive: {
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:false
+            }
+        }
+    });
+
+
+
+
+
+
+});
